e37aed534e19e67944bdce5f162e76af226c73b3
Muhammad Rafie Triyanto <79514755+coolraptor08@users.noreply.github.com>
2021-12-10 20:44:06 +0700

    Add background drawer

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
+const player=new create("player");
+
 function game()
 {
   ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.fillStyle="green";
-  ctx.fillRect(0,0,canvas.width,canvas.height)
+  drawBg();
   player.checkCollision();
   player.control();
   player.draw();
   requestAnimationFrame(game)
 }
-const player=new create("player");
 game();
+
+function drawBg()
+{
+	mapImage= new Image();
+	mapImage.src='image/bg.jpg';
+	ctx.drawImage(mapImage,0,0,canvas.width,canvas.height)
+}
