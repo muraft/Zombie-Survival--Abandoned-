309e5ba3ca8e9f157c3783abafa0cc0c32984c18
Muhammad Rafie Triyanto <79514755+coolraptor08@users.noreply.github.com>
2021-12-10 20:37:34 +0700

    Create objects.js
    -Replace the old units.js file
    -Adjust player's spawn position

---
+++ js/objects.js
@@ -0,0 +1,16 @@
+const units= (name,that) =>
+{
+	switch(name)
+	{
+		case "player":
+		that.x=0;
+		that.y=60;
+		that.width=30;
+		that.height=30;
+		that.color="blue";
+		that.image=new Image();
+		that.image.src='image/stickman.png';
+		that.step=30;
+		break;
+	}
+}
