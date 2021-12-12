6d4a9920b39019926ab44f8067a97ccb2ee91a12
Muhammad Rafie <79514755+muraft@users.noreply.github.com>
2021-12-12 22:01:58 +0700

    Add bullet
    -Add bullet const, it's an array that hold all the bullet generated
    -Add case "bullet" that create a bullet object to the game

--- js/objects.js
+++ js/objects.js
@@ -1,4 +1,6 @@
-const units= (name,that) =>
+const bullet=[];
+
+const objects= (name,that,x,y,width,height) =>
 {
 	switch(name)
 	{
@@ -8,9 +10,17 @@ const units= (name,that) =>
 		that.width=30;
 		that.height=30;
 		that.color="blue";
+		that.step=30;
 		that.image=new Image();
 		that.image.src='image/stickman.png';
-		that.step=30;
 		break;
+		
+		case "bullet":
+		that.x=x+width;
+		that.y=y+(height/5);
+		that.width=10;
+		that.height=2.5;
+		that.color="yellow";
+		that.speed=5;
 	}
 }
